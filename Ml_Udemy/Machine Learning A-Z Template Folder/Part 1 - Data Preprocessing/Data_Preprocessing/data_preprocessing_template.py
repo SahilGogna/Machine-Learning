2f02c41cb2a779d13898eba914a6d49e8d4d19b2
Sahil Gogna <gogna.sahil95@gmail.com>
2020-04-01 23:12:14 -0400

    Data preprocessing template completed.

--- Ml_Udemy/Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Data_Preprocessing/data_preprocessing_template.py
+++ Ml_Udemy/Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Data_Preprocessing/data_preprocessing_template.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 #importing the data
+# keep this for template
 dataset = pd.read_csv('Data.csv')
 x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
@@ -36,3 +37,17 @@ x = np.array(columnTransformer.fit_transform(x), dtype = np.str)
 
 labelEncoder_y = LabelEncoder()
 y = labelEncoder_y.fit_transform(y)
+
+#splitting the data into training set and test set
+#keep this for template
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.2, random_state = 0)
+
+
+#feature scaling
+#keep this for template
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
+
